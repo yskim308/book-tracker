956e6e8f9c1b6eb0c311612802a8ecd36a961893
Young Seo Kim <youngseokim7@gmail.com>
2025-04-18 15:44:35 +0900

    setup frontend to call bakend auth

--- next/src/app/signIn/page.tsx
+++ next/src/app/signIn/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 export default function SignInPage() {
+  const backendBase = process.env.NEXT_PUBLIC_BACKEND_BASE;
+
+  if (!backendBase) {
+    throw new Error("backend base environment variable not found");
+  }
+
+  const goToAuth = (authProvider: string) => {
+    window.location.href = `${backendBase}/auth/${authProvider}`;
+  };
+
   return (
     <div>
       <h1>hello from the sign in page</h1>
+      <button onClick={() => goToAuth("google")}>sign in with gogle</button>
     </div>
   );
 }
