fd2760089691d6bbc979f4b27fad211980e70e28
Young Seo Kim <youngseokim7@gmail.com>
2025-04-18 13:08:13 +0900

    intial auth

---
+++ express/auth/googleStrategy.ts
@@ -0,0 +1,36 @@
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import type { Profile } from "passport-google-oauth20";
+import passport from "passport";
+import type { DoneCallback } from "passport";
+
+const clientID = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+if (!clientID || !clientSecret) {
+  throw new Error("invalid or empty google client id or secret");
+}
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: clientID,
+      clientSecret: clientSecret,
+      callbackURL: "http://www.example.com/auth/google/callback",
+    },
+    function (
+      accessToken: string,
+      refreshToken: string,
+      profile: Express.User,
+      done,
+    ) {
+      return done(null, profile);
+    },
+  ),
+);
+
+passport.serializeUser((user: Express.User, done: DoneCallback) => {
+  return done(null, user);
+});
+
+passport.deserializeUser((user: Express.User, done: DoneCallback) => {
+  return done(null, user);
+});
