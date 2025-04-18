6af5a88af943e3de0c8539a29f2ce94ccfbc8a64
Young Seo Kim <youngseokim7@gmail.com>
2025-04-18 15:34:38 +0900

    add landing page and sign-in page routing

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+  const goToAuth = () => {
+    router.push("/signIn");
+  };
+
   return (
     <div>
       <h1>hello world from nextjs</h1>
+      <button onClick={goToAuth}>click here to signin</button>
     </div>
   );
 }
---
+++ next/src/app/signIn/page.tsx
@@ -0,0 +1,7 @@
+export default function SignInPage() {
+  return (
+    <div>
+      <h1>hello from the sign in page</h1>
+    </div>
+  );
+}
