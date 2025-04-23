84e6f7cfc41b045f492e2f7adfc5a2afa7b23e8f
Young Seo Kim <youngseokim7@gmail.com>
2025-04-23 14:16:45 +0900

    initial implementation of protected route

--- next/src/app/home/page.tsx
+++ next/src/app/home/page.tsx
@@ -1,4 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+  const backendBase = process.env.NEXT_PUBLIC_BACKEND_BASE;
+  const [message, setMesssage] = useState(null); // type explictily later
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${backendBase}/home`);
+        if (!response.ok) {
+          throw new Error("unauthorized"); // should route elsewhere to error page
+        }
+        const message = await response.json();
+        setMesssage(message);
+      } catch (e: unknown) {
+        console.log(e);
+        router.push("/error");
+      }
+    };
+
+    fetchData();
+  });
   return (
     <div>
       <h1>hello from homepage, check cookies please</h1>
