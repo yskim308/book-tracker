3d054c6cd41831d34c070e4cef8cc06161cf8d5a
Young Seo Kim <youngseokim7@gmail.com>
2025-04-26 18:17:45 +0900

    set state and submit for search form

--- next/src/components/BookSearchbar.tsx
+++ next/src/components/BookSearchbar.tsx
@@ -2,16 +2,23 @@ import { useState } from "react";
 
 export default function BookSearchbar() {
   const [value, setValue] = useState<string>("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setValue("");
+  };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
   return (
     <div>
-      <input
-        type="text"
-        placeholder="search for books"
-        value={value}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setValue(event.target.value)
-        }
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="search for books"
+          value={value}
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
 }
