1bab95c0e55569c91d822a01c4138af4437dfa16
Young Seo Kim <youngseokim7@gmail.com>
2025-04-27 15:26:24 +0900

    update types for books searches

--- next/src/types.ts
+++ next/src/types.ts
@@ -4,12 +4,29 @@ export interface User {
   name?: string;
   email?: string;
   picture?: string;
-  books: Book[];
+  books: UserBook[];
 }
 
-export interface Book {
+export interface UserBook {
   title: string;
   link: string;
   completionDate: string;
   status: "READ" | "READING" | "TO_READ";
+}
+
+export interface SearchBook {
+  id: string;
+  volumeInfo: {
+    title: string;
+    publisher: string;
+    publishedDate: string;
+    authors: string[];
+    description: string;
+    imageLinks: {
+      smallThumbnail: string;
+      thumbnail: string;
+    };
+    pageCount: number;
+    language: string;
+  };
 }
