a2b247612e1ab8ffef233f930e14998897c4b9f7
Young Seo Kim <youngseokim7@gmail.com>
2025-05-08 18:07:06 +0900

    update search container to pass in works key

--- next/src/components/searchbar/SearchContainer.tsx
+++ next/src/components/searchbar/SearchContainer.tsx
@@ -13,7 +13,7 @@ export default function SearchContainer({ books }: SearchContainerProps) {
     <div className="z-50 absolute bg-white border border-black rounded-3xl left-1/2 -translate-x-1/2 mt-2 w-10/12 lg:w-1/2 p-5">
       {books.length ? (
         books.map((book: SearchBook) => (
-          <SearchSuggestion key={book.id} book={book} />
+          <SearchSuggestion key={book.key} book={book} />
         ))
       ) : (
         <h1>empty</h1>
