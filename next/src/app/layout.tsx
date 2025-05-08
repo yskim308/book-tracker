ceafdf09927066aead4ee1ab5024ba61fc0a00cd
Young Seo Kim <youngseokim7@gmail.com>
2025-05-08 18:46:23 +0900

    add radix ui themes

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Theme } from "@radix-ui/themes";
 import "./globals.css";
 import BookSearchbar from "@/components/searchbar/BookSearchbar";
 import Profile from "@/components/profile/Profile";
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }
