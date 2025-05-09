4bbc9d9a4a55dcf09f9b5036dc2c1077609bface
Young Seo Kim <youngseokim7@gmail.com>
2025-05-09 15:45:59 +0900

    update width to match searchbar

--- next/src/components/searchbar/SearchContainer.tsx
+++ next/src/components/searchbar/SearchContainer.tsx
@@ -11,8 +11,8 @@ export default function SearchContainer({ books }: SearchContainerProps) {
     return null;
   }
   return (
-    <div className="z-50 absolute bg-white border border-black rounded-3xl left-1/2 -translate-x-1/2 mt-2 w-10/12 lg:w-1/2 p-5 h-1/2">
-      <ScrollArea>
+    <div className="z-50 absolute bg-white border border-black rounded-3xl left-1/2 -translate-x-1/2 mt-2 w-3/4 lg:w-1/4 p-5">
+      <ScrollArea className="max-h-1/4">
         {books.length ? (
           books
             .slice(0, 10)
