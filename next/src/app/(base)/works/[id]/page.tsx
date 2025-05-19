6b78128aa51dd0032908a37800e52b7d0e028e5c
Young Seo Kim <youngseokim7@gmail.com>
2025-05-19 14:20:19 +0900

    import work detail component into work page

--- next/src/app/(base)/works/[id]/page.tsx
+++ next/src/app/(base)/works/[id]/page.tsx
@@ -2,6 +2,7 @@
 import type { Work, Author, Edition } from "@/types";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import WorkDetail from "@/components/workpage/WorkDetail";
 
 export default function Page() {
   const params = useParams();
@@ -9,7 +10,6 @@ export default function Page() {
 
   const apiEndpoint = process.env.NEXT_PUBLIC_BOOK_SEARCH_BASE;
   const [work, setWork] = useState<Work | null>(null);
-  const [authors, setAuthors] = useState<Author[] | null>(null);
   const [editions, setEditions] = useState<Edition[] | null>(null);
 
   // getting the work on reload
@@ -27,34 +27,12 @@ export default function Page() {
     getWork();
   }, []);
 
-  // getting tue author info when work changes
-  useEffect(() => {
-    const getAuthor = async () => {
-      try {
-        const promises = work.authors.map(async (authorObject) => {
-          const response = await fetch(
-            `${apiEndpoint}${authorObject.author.key}.json`,
-          );
-          return await response.json();
-        });
-
-        const fetchedData: Author[] = await Promise.all(promises);
-        setAuthors(fetchedData);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    if (work && work.authors) {
-      getAuthor();
-    }
-  }, [work]);
-
   return (
     <div>
       <h1>from book page:</h1>
       <h1>{work?.title}</h1>
-      <h1>{work?.authors[0].author.key}</h1>
+      <h1>work detail component</h1>
+      {work && <WorkDetail work={work} />}
     </div>
   );
 }
