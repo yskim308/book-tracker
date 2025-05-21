324429b1b49e71393fcdccf704f37e2f4a07f6a8
Young Seo Kim <youngseokim7@gmail.com>
2025-05-21 17:27:59 +0900

    edit spacing between components in workdetail

--- next/src/components/workpage/WorkDetail.tsx
+++ next/src/components/workpage/WorkDetail.tsx
@@ -13,12 +13,12 @@ export default function WorkDetail({ work }: WorkDetailProps) {
       <div className="flex justify-center px-4">
         <Covers keys={work.covers} />
       </div>
-      <div className="flex flex-col space-y-6 px-4">
+      <div className="flex flex-col space-y-3 px-4">
         <div>
           <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
             {work.title}
           </h1>
-          <div className="mt-4">
+          <div className="mt-1">
             <h2 className="text-lg font-medium text-slate-700">Authors</h2>
             <Author authors={work.authors} />
           </div>
@@ -26,7 +26,7 @@ export default function WorkDetail({ work }: WorkDetailProps) {
 
         <div>
           <h2 className="text-lg font-medium text-slate-700">Description</h2>
-          <div className="mt-2 prose prose-slate max-w-none">
+          <div className="prose prose-slate max-w-none">
             {typeof work.description === "string" ? (
               <p>{work.description}</p>
             ) : (
