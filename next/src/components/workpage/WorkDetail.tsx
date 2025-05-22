8e67eed7531daec311576e8d64a20bfc4f595755
Young Seo Kim <youngseokim7@gmail.com>
2025-05-22 14:12:15 +0900

    change cover and descriptions to be under a flex-col div

--- next/src/components/workpage/WorkDetail.tsx
+++ next/src/components/workpage/WorkDetail.tsx
@@ -13,28 +13,31 @@ export default function WorkDetail({ work }: WorkDetailProps) {
   return (
     //todo fix scroll area
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-      <div className="flex justify-center px-4">
+      <div className="flex-col items-center px-4">
         <Covers keys={work.covers} />
-      </div>
-      <ScrollArea className="h-72">
-        <div className="flex flex-col space-y-3 px-4">
-          <div>
-            <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
-              {work.title}
-            </h1>
-            <div className="mt-1">
-              <h2 className="text-lg font-medium text-slate-700">Authors</h2>
-              <Author authors={work.authors} />
+        <ScrollArea className="h-72">
+          <div className="flex flex-col space-y-3 px-4">
+            <div>
+              <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+                {work.title}
+              </h1>
+              <div className="mt-1">
+                <h2 className="text-lg font-medium text-slate-700">Authors</h2>
+                <Author authors={work.authors} />
+              </div>
             </div>
+
+            <Description description={work.description} />
+
+            {work.subjects && work.subjects.length > 0 && (
+              <Subjects subjects={work.subjects} />
+            )}
           </div>
-
-          <Description description={work.description} />
-
-          {work.subjects && work.subjects.length > 0 && (
-            <Subjects subjects={work.subjects} />
-          )}
-        </div>
-      </ScrollArea>
+        </ScrollArea>
+      </div>
+      <div>
+        <h1>hello world</h1>
+      </div>
     </div>
   );
 }
