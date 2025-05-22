59298be2b371f2bddb28d829f274c6caff77a995
Young Seo Kim <youngseokim7@gmail.com>
2025-05-22 23:02:30 +0900

    use ring instead of box shadow

--- next/src/components/workpage/WorkDetail.tsx
+++ next/src/components/workpage/WorkDetail.tsx
@@ -13,7 +13,7 @@ export default function WorkDetail({ work }: WorkDetailProps) {
   return (
     //todo fix scroll area
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-      <div className="flex-col items-center px-4 shadow-md shadow-blue-100 py-2 rounded-lg">
+      <div className="flex-col items-center px-4 ring-4 ring-blue-50 py-2 rounded-lg">
         <Covers keys={work.covers} />
         <ScrollArea className="h-[300px] md:h-[450px] mt-5">
           <div className="flex flex-col space-y-3 px-4">
