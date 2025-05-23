44aa4398c252a8a3a1717fc7ded4ffdc4ca35214
Young Seo Kim <youngseokim7@gmail.com>
2025-05-23 15:31:42 +0900

    edit height to be 500 on big screens

--- next/src/components/workpage/WorkDetail.tsx
+++ next/src/components/workpage/WorkDetail.tsx
@@ -15,7 +15,7 @@ export default function WorkDetail({ work }: WorkDetailProps) {
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
       <div className="flex-col items-center px-4 ring-4 ring-blue-50 py-2 rounded-lg">
         <Covers keys={work.covers} />
-        <ScrollArea className="h-[300px] md:h-[450px] mt-5">
+        <ScrollArea className="h-[300px] md:h-[500px] mt-5">
           <div className="flex flex-col space-y-3 px-4">
             <div>
               <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
