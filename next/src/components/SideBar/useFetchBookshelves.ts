8b548dba82faccac57e89e96ca24fd85e438e363
Young Seo Kim <youngseokim7@gmail.com>
2025-05-27 16:12:33 +0900

    create hook for fetching bookshelves for the sidebar

---
+++ next/src/components/SideBar/useFetchBookshelves.ts
@@ -0,0 +1,52 @@
+"use client";
+
+import { useUserState } from "@/context/UserContext";
+import { useEffect, useState } from "react";
+
+interface Bookshelf {
+  name: string;
+  count: number;
+}
+
+export default function SideBarContainer() {
+  const { user, loading } = useUserState();
+  const [bookshelves, setBookshelves] = useState<Bookshelf[] | null>(null);
+  const [shelfLoading, setShelfLoading] = useState<boolean>(true);
+
+  const backendBase = process.env.NEXT_PUBLIC_BACKEND_BASE;
+  if (!backendBase) {
+    throw new Error("backend env variable not setup");
+  }
+
+  useEffect(() => {
+    const getShelves = async () => {
+      try {
+        if (loading) {
+          return;
+        }
+        const response = await fetch(`${backendBase}/bookshelves`, {
+          credentials: "include",
+        });
+        if (!response.ok) {
+          console.error("error with getting api: ", response);
+          return;
+        }
+        const data = await response.json();
+        setBookshelves(data.bookshelves);
+      } catch (e) {
+        console.error("error with api: " + e);
+      } finally {
+        setShelfLoading(false);
+      }
+    };
+
+    getShelves();
+  }, [loading, backendBase]); // Added dependencies
+
+  return {
+    bookshelves,
+    shelfLoading,
+    user,
+    loading,
+  };
+}
