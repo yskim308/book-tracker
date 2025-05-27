e6e67cdf356a1ea66983588db6baabf50e885184
Young Seo Kim <youngseokim7@gmail.com>
2025-05-27 15:34:55 +0900

    wrap base layout with user context provider

--- next/src/app/(base)/layout.tsx
+++ next/src/app/(base)/layout.tsx
@@ -1,18 +1,21 @@
 import BookSearchbar from "@/components/searchbar/BookSearchbar";
 import Profile from "@/components/profile/Profile";
+import { UserProvider } from "@/context/UserContext";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div>
-      <div className="flex justify-center relative my-5">
-        <BookSearchbar />
-        <div className="absolute right-0 mx-2 md:mx-5 flex justify-center">
-          <Profile />
+      <UserProvider>
+        <div className="flex justify-center relative my-5">
+          <BookSearchbar />
+          <div className="absolute right-0 mx-2 md:mx-5 flex justify-center">
+            <Profile />
+          </div>
         </div>
-      </div>
-      <div>{children}</div>
+        <div>{children}</div>
+      </UserProvider>
     </div>
   );
 }
