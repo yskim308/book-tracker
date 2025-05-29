777eeebf2a7357f70ed044c1b89da292615a93bc
Young Seo Kim <youngseokim7@gmail.com>
2025-05-29 13:17:11 +0900

    add sidebar to layout

--- next/src/app/(base)/layout.tsx
+++ next/src/app/(base)/layout.tsx
@@ -1,21 +1,31 @@
+import type React from "react";
 import BookSearchbar from "@/components/searchbar/BookSearchbar";
 import Profile from "@/components/profile/Profile";
 import { UserProvider } from "@/context/UserContext";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/SideBar/Sidebar";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div>
-      <UserProvider>
-        <div className="flex justify-center relative my-5">
-          <BookSearchbar />
-          <div className="absolute right-0 mx-2 md:mx-5 flex justify-center">
-            <Profile />
+    <UserProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="flex-1">
+          {/* Header with search and profile */}
+          <div className="flex justify-center relative my-5 px-4">
+            <SidebarTrigger className="absolute left-0 top-1/2 -translate-y-1/2" />
+            <BookSearchbar />
+            <div className="absolute right-0 mx-2 md:mx-5 flex justify-center">
+              <Profile />
+            </div>
           </div>
-        </div>
-        <div>{children}</div>
-      </UserProvider>
-    </div>
+
+          {/* Main content */}
+          <div className="px-4">{children}</div>
+        </main>
+      </SidebarProvider>
+    </UserProvider>
   );
 }
